e25c2e3917153266818819b22ce2ca948efdd99c
Luis Concha <lconcha@gmail.com>
2024-05-03 18:48:58 -0600

    write to tempdir first

--- my_fba_simplemetrics.sh
+++ my_fba_simplemetrics.sh
@@ -8,4 +8,12 @@ mask=${FBA_DIR}/${subj}/mask.mif
 fa=${FBA_DIR}/${subj}/fa.mif
 v1=${FBA_DIR}/${subj}/v1.mif
 
-dwi2tensor -mask $mask $dwis - | tensor2metric -fa $fa -vector $v1 -
+tmpDir=$(mktemp -d)
+
+#dwi2tensor -mask $mask $dwis - | tensor2metric -fa $fa -vector $v1 -
+dwi2tensor -mask $mask $dwis - | tensor2metric -fa ${tmpDir}/fa.mif -vector ${tmpDir}/v1.mif -
+
+cp  ${tmpDir}/fa.mif $fa
+cp  ${tmpDir}/v1.mif $v1
+
+rm -fR $tmpDir
